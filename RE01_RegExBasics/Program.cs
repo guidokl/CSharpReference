e3c6250d3bdac5199e6c502c4dc3ec9e08f5fa20
Guido Kleine <guidokl@pm.me>
2026-01-04 22:35:42 +0100

    Add RE01_RegExBasics

---
+++ RE01_RegExBasics/Program.cs
@@ -0,0 +1,11 @@
+﻿// https://learn.microsoft.com/en-us/dotnet/standard/base-types/regular-expressions
+// https://learn.microsoft.com/en-us/dotnet/standard/base-types/regular-expression-language-quick-reference
+
+using System.Text.RegularExpressions;
+
+string pattern = "(Mr\\.? |Mrs\\.? |Miss |Ms\\.? )";
+string[] names = [ "Mr. Henry Hunt", "Ms. Sara Samuels", 
+                   "Abraham Adams", "Ms. Nicole Norris" ];
+
+foreach (string name in names)
+    Console.WriteLine(Regex.Replace(name, pattern, String.Empty));
